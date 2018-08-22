09e99dffeb68eab64b8fcb8bd76bc065f5c5bdf4
Edwin Boyko <>
2018-08-22 22:48:22 +0100

    initial commit

---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by Edwin Boyko on 22/08/2018.
+//  Copyright © 2018 Edwin Boyko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
